d8b71a7c1d6f439f8443f6a8a82e070cafcc763c
pradheepa <pradheepa.p@gmail.com>
2020-11-12 19:22:49 +0800

    Local app working version

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
+const serverless = require('serverless-http');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
+var blogRouter = require('./routes/blog');
+var contactRouter = require('./routes/contact');
+var menuRouter = require('./routes/menu');
+
 var app = express();
 
-//view engine setup
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public', 'app')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/app/index.html'));
-});
+app.use('/index.html', indexRouter);
+app.use('/', indexRouter);
+app.use('/menu.html', menuRouter);
+app.use('/blog.html', blogRouter);
+app.use('/contact.html', contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,5 +43,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// module.exports.handler = serverless(app);
 
 module.exports = app;
